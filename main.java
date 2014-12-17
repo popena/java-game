ec214ad50b5b7f3c6fad24079d81f42f67b36240
popena <miko-gun@hotmail.com>
2014-12-17 18:53:22 +0200

    first commit

---
+++ main.java
@@ -0,0 +1,18 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		Peli f = new Peli(600,500);
+		f.setPreferredSize(new Dimension(600,500));
+		f.setSize(600,500);
+		f.setVisible(true);
+		//f.show();
+		
+
+	}
+
+}
