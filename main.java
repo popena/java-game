c51f279c99fcd347d35092b70c1eca4449f73ce9
elgis <aaro.a7@hotmail.com>
2014-12-17 20:04:28 +0200

    optimizations

--- main.java
+++ main.java
@@ -6,13 +6,9 @@ import javax.swing.JFrame;
 public class main {
 
 	public static void main(String[] args) {
-		Peli f = new Peli(600,500);
+		Game f = new Game(600,500);
 		f.setPreferredSize(new Dimension(600,500));
 		f.setSize(600,500);
-		f.setVisible(true);
-		//f.show();
-		
-
+		f.setVisible(true);	
 	}
-
 }
